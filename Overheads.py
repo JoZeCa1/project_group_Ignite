d1c5410d7b358bf65d4b95d633af9dc1d9d2d724
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-01-28 08:49:51 +0800

    Update Overheads.py

--- Overheads.py
+++ Overheads.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import csv
 
-file_path_overheads=Path.cwd()/'project_group_Ignite'/'csv_report_Ignite'/'Overheads1.csv'
-file_path_overheads.touch()
+file_path_overheads=Path.cwd()/'csv_report_Ignite'/'Overheads1.csv'
+# file_path_overheads.touch()
 # print(file_path_overheads.exists())
 
 with file_path_overheads.open(mode='r',encoding='utf-8') as file:
@@ -10,28 +10,51 @@ with file_path_overheads.open(mode='r',encoding='utf-8') as file:
     next(reader)
 
     
+    Cluster5=[]
     Cluster6=[]
-
     for num in reader:
+        Cluster5.append(num)
+   
+#
+# Converted5=[[x[0],float(x[1])] for x in Cluster5]
+Converted5=[]
+for x in Cluster5:
+    Convertedx=[x[0],float(x[1])]
+    Converted5.append(Convertedx)
+
+
+testerbot2=[]
+testerbot=[]
+
+def overhead():
+    global testerbot2
+    global testerbot
+    day=0
+    dayed=1
+    testerbot.append(Converted5[0])
+    
+    while day<len(Converted5):
+        day+=1 
         
-        Cluster6.append(num)
-
-
-Cluster6.sort(key=lambda value:value[1],reverse=True)
-highest_overhead=[]
-highest_overhead.append(f'[HIGHEST OVERHEADS] {Cluster6[0][0]}: {Cluster6[0][1]}')
-
-#this is 2nd method if cannot use lambda
-# def sort(Cluster6):
-#     length = len(Cluster6)
-#     for num in range(0, length):
-#         for num1 in range(0, length-num-1):
-#             if (Cluster6[num1][1] > Cluster6[num1 + 1][1]):
-#                 sum = Cluster6[num1]
-#                 Cluster6[num1]= Cluster6[num1 + 1]
-#                 Cluster6[num1 + 1]= sum
-#     return Cluster6
-
+        if day<len(Converted5):
+            
+            
+            if Converted5[day][dayed]>testerbot[0][1]:
+                
+                testerbot.pop(0)
+                testerbot.append(Converted5[day])
+                
+                if day==len(Converted5)-1:
+                    testerbot2.append(f'[Highest Expense] {testerbot}')
+                    return(testerbot2)
+                
+            else:
+                if day==len(Converted5)-1:
+                    testerbot2.append(f'[Highest Expense] {testerbot[0][0]} {testerbot[0][1]}%')
+                    return(testerbot2)
+                
+            
+print(overhead())
 
                 
 
