e1a4d626821bfcacbbd401c99214ed1a729c70c6
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-01-28 08:49:12 +0800

    Update Profit_Loss.py

--- Profit_Loss.py
+++ Profit_Loss.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import csv
 
-filepath2=Path.cwd()/'project_group_Ignite'/'csv_report_Ignite'/'Profit&Loss1.csv'
-filepath2.touch()
-#print(filepath2.exists())
+filepath2=Path.cwd()/'csv_report_Ignite'/'Profit&Loss1.csv'
+
 
 with filepath2.open(mode='r',encoding='utf-8') as file:
     reader=csv.reader(file)
@@ -25,27 +24,50 @@ converted4 = [float(num) for num in cluster2]
 
 
 
-one1=1
-one=0
-days=0
 pl_deficit=[]
 pl_surplus=[]
-pl_surplus_all=[]
+pl_surplus_all=[]      
 
-while days < len(converted4):
-    days+=1
+# print(len(converted2))
+def conversion1():
+    one1=1
+    one=0
+    days=0
+    global pl_deficit, pl_surplus,pl_surplus_all
     
-    if days<len(converted4):
-        if converted4[one]>converted4[one1]:
-            pl_deficit.append(f'[Profit Deficit] Day: {converted3[days]}, AMOUNT: ${converted4[one]-converted4[one1]}')
-        
-            one+=1
-            one1+=1
-    
-        else:
-            pl_surplus.append(f'{converted3[days]}')
-            one+=1
-            one1+=1
-            if len(pl_surplus)==len(converted3)-1:
-                pl_surplus_all.append(f'[Net Profit SURPLUS] PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
 
+    while days < len(converted4):
+        days+=1
+        # print(days)
+        if days<len(converted4):
+            # print(days)
+            if converted4[one]>converted4[one1]:
+
+                pl_deficit.append(f'[Profit Deficit] Day: {converted3[days]}, AMOUNT: ${converted4[one]-converted4[one1]}')
+                
+                if days == len(converted4)-1:
+                    return(pl_deficit)
+                else:
+                    one+=1
+                    one1+=1
+                
+
+            else :
+
+                pl_surplus.append(f'{converted3[days]}')
+
+                if len(pl_surplus)==len(converted4)-1:
+                    # print(len(cash_surplus))
+                    # print(len(converted2))
+                    pl_surplus_all.append(f'[PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
+
+                    return(pl_surplus_all)
+
+                    
+                else:
+                   one+=1
+                   one1+=1 
+                   
+
+            
+print(conversion1())
