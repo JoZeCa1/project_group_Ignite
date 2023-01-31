9740e218c43da3b417e4f809d221f3133cd62b33
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-01-31 16:46:58 +0800

    Update Overheads.py

--- Overheads.py
+++ Overheads.py
@@ -1,72 +1,101 @@
+# Import Path method from pathlib
 from pathlib import Path
 import csv
 
-file_path_overheads=Path.cwd()/'csv_report_Ignite'/'Overheads1.csv'
-# file_path_overheads.touch()
-# print(file_path_overheads.exists())
+# instantiate an file path object to current working directory
+file_path_overheads=Path.cwd()/'csv_report_Ignite'/'Overheads.csv'
 
+# create a new file with `.touch()`
+file_path_overheads.touch()
+
+# open file_path_overheads with .open() to return a file object
 with file_path_overheads.open(mode='r',encoding='utf-8') as file:
+
+    # instantiate a reader object
     reader=csv.reader(file)
+
+    # use `next()` to skip the header.
     next(reader)
 
-    
+    # Create an empty list called Cluster5
     Cluster5=[]
-    Cluster6=[]
+
+    #Using a for loop extract data from reader and append into CLuster5
     for num in reader:
         Cluster5.append(num)
    
-#
-# Converted5=[[x[0],float(x[1])] for x in Cluster5]
+#Create empty list called Converted5
 Converted5=[]
+
+#Convert the percentage values Cluster5 into a float form String and append into Converted5
 for x in Cluster5:
     Convertedx=[x[0],float(x[1])]
     Converted5.append(Convertedx)
 
-
-testerbot2=[]
-testerbot=[]
+#Create two empty lists
+Overheads=[]
+Overheads2=[]
 
 def overhead():
-    global testerbot2
-    global testerbot
-    day=0
-    dayed=1
-    testerbot.append(Converted5[0])
+
+    """
+    This function helps the team find the highest overhead category
+
+    """
+    #extract the 2 global scope empty list using global method into a local scope
+    global Overheads
+    global Overheads2
+
+    # Create 2 variables that will be used later in the function
+    num_1=0
+    num_2=1
+
+    # Append the first value form Converted5 into Overheads2 which will be used for comparision purposes later in the function
+    Overheads2.append(Converted5[0])
     
-    while day<len(Converted5):
-        day+=1 
+    #create a while loop
+    while num_1<len(Converted5):
+
+        # add a value of 1 to num_1 variable
+        num_1+=1 
         
-        if day<len(Converted5):
+        # Create an if statement
+        if num_1<len(Converted5):
             
-            
-            if Converted5[day][dayed]>testerbot[0][1]:
+            # Check if the value in Converted5 is greater than value of Values in the overheads2
+            if Converted5[num_1][num_2]>Overheads2[0][1]:
                 
-                testerbot.pop(0)
-                testerbot.append(Converted5[day])
+                # If the requirements above are met, remove the items in the Overheads2
+                Overheads2.pop(0)
+
+                # Append the Greater Value into Overheads2
+                Overheads2.append(Converted5[num_1])
                 
-                if day==len(Converted5)-1:
-                    testerbot2.append(f'[Highest Expense] {testerbot}')
-                    return(testerbot2)
+                # Once all the values in Converted5 has gone through the function
+                if num_1==len(Converted5)-1:
+
+                    # Append the Highest Overhead into the lsit Overhead
+                    Overheads.append(f'[Highest Expense] {Overheads2}')
+
+                    # Return to receive results
+                    return(Overheads)
                 
+                #Else statement is not required as code should run only if the statement above is true
+
+            # If it does not meet the requirements above    
             else:
-                if day==len(Converted5)-1:
-                    testerbot2.append(f'[Highest Expense] {testerbot[0][0]} {testerbot[0][1]}%')
-                    return(testerbot2)
-                
+
+                # Once all the values in Converted5 has gone through the function
+                if num_1==len(Converted5)-1:
+
+                    # Append the Highest Overhead into the lsit Overhead
+                    Overheads.append(f'[Highest Expense] {Overheads2[0][0]} {Overheads2[0][1]}%')
+
+                    # Return to receive results
+                    return(Overheads)
+
+                #Else statement is not required as code should run only if the statement above is true
+        else:
+            return(Overheads)        
             
 print(overhead())
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
