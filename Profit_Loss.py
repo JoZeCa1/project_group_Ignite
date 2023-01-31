ce846e56a2b0eebe6be63b8718a76df1f2738c89
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-01-31 16:35:52 +0800

    Update Profit_Loss.py

--- Profit_Loss.py
+++ Profit_Loss.py
@@ -1,30 +1,36 @@
-# import Path 
+# Import Path method from pathlib 
 from pathlib import Path
+
 # import spreadsheet
 import csv
 
-# create a file to csv file
-filepath2=Path.cwd()/'csv_report_Ignite'/'Profit&Loss1.csv'
+# instantiate an file path object to current working directory
+filepath2=Path.cwd()/'csv_report_Ignite'/'Profit&Loss.csv'
 
-# read the csv file to append profit and loss from the cvs
+# create a new file with `.touch()`
+filepath2.touch()
+
+
 with filepath2.open(mode='r',encoding='utf-8') as file:
+
+    
     reader=csv.reader(file)
-    # skip header
+
+    
     next(reader)
     
-    # create 2 empty lists to store profit and loss by each cluster
-    cluster1=[]
-    cluster2=[]
+
+    cluster3=[]
+    cluster4=[]
 
     for day,sales,profit,op,profitloss in reader:
-        cluster1.append(day)
-        cluster2.append(profitloss)
+        cluster3.append(day)
+        cluster4.append(profitloss)
 
-    # print(cluster2)
-    # print(cluster1)
+    
 
-converted3 = [float(num) for num in cluster1]
-converted4 = [float(num) for num in cluster2]
+converted3 = [float(num) for num in cluster3]
+converted4 = [float(num) for num in cluster4]
 
 
 
@@ -32,46 +38,51 @@ pl_deficit=[]
 pl_surplus=[]
 pl_surplus_all=[]      
 
-# print(len(converted2))
+
 def conversion1():
-    one1=1
-    one=0
-    days=0
+    """
+    Pls add DocStrings TY
+
+    """
+    num_1=1
+    num_2=0
+    num_3=0
+    none=[]
     global pl_deficit, pl_surplus,pl_surplus_all
     
 
-    while days < len(converted4):
-        days+=1
-        # print(days)
-        if days<len(converted4):
-            # print(days)
-            if converted4[one]>converted4[one1]:
+    while num_3 < len(converted4):
+        num_3+=1
+        
+        if num_3<len(converted4):
+            
+            if converted4[num_2]>converted4[num_1]:
 
-                pl_deficit.append(f'[Profit Deficit] Day: {converted3[days]}, AMOUNT: ${converted4[one]-converted4[one1]}')
+                pl_deficit.append(f'[Profit Deficit] Day: {converted3[num_3]}, AMOUNT: ${converted4[num_2]-converted4[num_1]}')
                 
-                if days == len(converted4)-1:
+                if num_3 == len(converted4)-1:
                     return(pl_deficit)
                 else:
-                    one+=1
-                    one1+=1
+                    num_1+=1
+                    num_2+=1
                 
 
             else :
 
-                pl_surplus.append(f'{converted3[days]}')
+                pl_surplus.append(f'{converted3[num_3]}')
 
                 if len(pl_surplus)==len(converted4)-1:
-                    # print(len(cash_surplus))
-                    # print(len(converted2))
+                    
                     pl_surplus_all.append(f'[PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
 
                     return(pl_surplus_all)
 
                     
                 else:
-                   one+=1
-                   one1+=1 
-                   
+                   num_1+=1
+                   num_2+=1 
+        else:
+            return(None)           
 
             
 print(conversion1())
