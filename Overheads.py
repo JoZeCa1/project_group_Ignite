2a56377582f6cd57657562d79e587bee51d12529
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-02-02 08:32:47 +0800

    Update Overheads.py

--- Overheads.py
+++ Overheads.py
@@ -26,28 +26,20 @@ with file_path_overheads.open(mode='r',encoding='utf-8') as file:
    
 # Create empty list called Converted5.
 Converted5=[]
-
 # Convert the percentage values from Cluster5 into a float form String and append into Converted5.
 for x in Cluster5:
     Convertedx=[x[0],float(x[1])]
     Converted5.append(Convertedx)
 
-# Create two empty lists.
-Overheads=[]
-Overheads2=[]
-
-# def function.
-def overhead():
-  
-    # Docstrings is considered as the comment. Briefly describes the function of this code.
-    """
+def overhead_function():
+    file_path=Path.cwd()/'summary_report.txt'
     
-    This function helps the team find the highest overhead category
-
     """
-    # Extract the 2 global scope empty list using global method into a local scope.
-    global Overheads
-    global Overheads2
+    This function helps the team find the highest overhead category
+    """
+    #Create 2 empty List
+    Overheads=[]
+    Overheads2=[]
 
     # Create 2 variables that will be used later in the function.
     num_1=0
@@ -76,28 +68,24 @@ def overhead():
                 
                 # Once all the values in Converted5 has gone through the function.
                 if num_1==len(Converted5)-1:
-
-                    # Append the Highest Overhead into the lsit Overhead.
-                    Overheads.append(f'[Highest Expense] {Overheads2}')
-
+                    # Append the Highest Overhead into the list Overhead.
+                    Overheads=(f'[HIGHEST OVERHEAD] {Overheads2}')
+                    #Write Highest Overhead into a summary test report
+                    with file_path.open(mode='w',encoding='utf-8') as file:
+                        file.write(Overheads+'\n')
                     # Return to receive results.
                     return(Overheads)
                 
-            # Else statement is not required as code should run only if the statement above is true.
             # If it does not meet the requirements above.    
             else:
-
                 # Once all the values in Converted5 has gone through the function.
                 if num_1==len(Converted5)-1:
-
-                    # Append the Highest Overhead into the lsit Overhead.
-                    Overheads.append(f'[Highest Expense] {Overheads2[0][0]} {Overheads2[0][1]}%')
-
+                    # Append the Highest Overhead into the list Overhead.
+                    Overheads=(f'[HIGHEST OVERHEAD] {Overheads2[0][0]} {Overheads2[0][1]}%')
+                    #Write Highest Overhead into a summary test report
+                    with file_path.open(mode='w',encoding='utf-8') as file:
+                        file.write(Overheads+'\n')
                     # Return to receive results.
                     return(Overheads)
-
-        # Else statement is not required as code should run only if the statement above is true.
-        else:
-            return(Overheads)        
             
-print(overhead())
+print(overhead_function())
