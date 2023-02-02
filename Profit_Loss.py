48e3d6b6290bd420612b531b200eda513ba700d0
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-02-02 09:02:06 +0800

    Update Profit_Loss.py

--- Profit_Loss.py
+++ Profit_Loss.py
@@ -1,101 +1,99 @@
-# Import Path method from pathlib.
+# Import Path method from pathlib 
 from pathlib import Path
 
-# Import spreadsheet.
+# import spreadsheet
 import csv
 
-# Instantiate an file path object to current working directory.
+# instantiate an file path object to current working directory
 filepath2=Path.cwd()/'csv_report_Ignite'/'Profit&Loss.csv'
 
-# Create a new file with `.touch()`.
+# create a new file with `.touch()`
 filepath2.touch()
 
-# read the csv file to append the profit and loss from the csv.
+# read the csv file to append profit and loss from the cvs
 with filepath2.open(mode='r',encoding='utf-8') as file:
     reader=csv.reader(file)
-    # skip header 
+
+    # skip header
     next(reader)
     
-    # create 2 empty lists to store profit and loss by each cluster
+    #Create 2 empty List
     cluster3=[]
     cluster4=[]
-    
-    # append profit and loss as a list back to each empty list
+
+    #Append only the Profit_Loss and Day from the Csv
     for day,sales,profit,op,profitloss in reader:
         cluster3.append(day)
         cluster4.append(profitloss)
 
-# convert the numbers in each cluster into float
-converted3 = [float(num) for num in cluster3]
-converted4 = [float(num) for num in cluster4]
+#Create 2 empty list
+converted3 = []
+converted4 = [] 
+#converted the days and cash from string to a float and placed day in converted 1 and profit/loss into converted 2
+for num in cluster3:
+    converteda=float(num)
+    converted3.append(converteda)
+for num in cluster4:
+    convertedb=float(num)
+    converted4.append(convertedb)
 
-# create 3 new clusters
-pl_deficit=[]
-pl_surplus=[]
-pl_surplus_all=[]      
-
-# def function 
-def conversion1():
-    
-    # Docstrings is considered as the comment. Briefly describes the function of this code.
+def Profit_loss_function():
     """
-    
     The program will compute the difference in the net profit column if net profit on the current day is lower than the previous day
-    
+    Write it into the summary_report.txt to be activated in the main.py
     """
-    
-    # creating 3 new variables
+    #Create a file path and create the summary_report.txt
+    file_path=Path.cwd()/'summary_report.txt'
+    file_path.touch()
+
+    #Create 3 variables
     num_1=1
     num_2=0
     num_3=0
-    none=[]
-    global pl_deficit, pl_surplus,pl_surplus_all
+
+    #Create 3 empty List
+    pl_deficit=[]
+    pl_surplus=[]
+    pl_surplus_all=[] 
     
-    # while num_3 is lesser than. len() function returns the number of items in converted 4 variable. 
+    #Create a while loop
     while num_3 < len(converted4):
-        # += adds the value of the right operand to a variable and assigns the result to the variable.
+        #Ensure that the while loop continues until target is met
         num_3+=1
-        
-        # if function
+
+        #Create an if statement
         if num_3<len(converted4):
-            
-            # if function. if num_2 in variable converted4 is larger than num_1 in variable converted4.
+            #Check if the Profit/Loss on the current day is greater/less than the Profit/Loss the following day
             if converted4[num_2]>converted4[num_1]:
-                
-                # append the variable pl_deficit, which appends a new element at the end of a list. 
+                #If there is a Profit deficit the programme will detect it and  append it into the list pl_deficit
                 pl_deficit.append(f'[Profit Deficit] Day: {converted3[num_3]}, AMOUNT: ${converted4[num_2]-converted4[num_1]}')
-                
-                # if function. if num_3 is equal to len() of variable converted4 minus 1. 
-                if num_3 == len(converted4)-1: 
+                #Check if all the values have gone through the while loop
+                if num_3 == len(converted4)-1:
+                    #Append the results to summary text report
+                    with file_path.open(mode='a',encoding='utf-8') as file1:
+                        for deficit in pl_deficit:
+                            file1.write(deficit +'\n')
+                    #Return to receive a value
                     return(pl_deficit)
-                
-                # else function
                 else:
+                    # add a value of 1 to num_2 and num_1 variable
                     num_1+=1
-                    num_2+=1
-                    
-            # else function
-            else:
-                
-                # append the variable pl_surplus, which appends a new element at the end of a list.
+                    num_2+=1   
+            else :
+                #Append into pl_surplus
                 pl_surplus.append(f'{converted3[num_3]}')
-                
-                # if function. if len() pl_surplus is equal to len () of converted4 minus 1. 
+                #If the length of pl_surplus is equal the length of converted 4 then there is profit surplus everyday
                 if len(pl_surplus)==len(converted4)-1:
                     
-                    # append the variable  pl_surplus_all, which appends a new element at the end of a list.
-                    pl_surplus_all.append(f'[PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
-                    
-                    # return function
-                    return(pl_surplus_all)
-
-                # else function 
+                    pl_surplus_all=(f'[PROFIT SURPLUS] PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
+                    #Append the results to summary text report
+                    with file_path.open(mode='a',encoding='utf-8') as file1:
+                        file1.write(pl_surplus_all +'\n')
+                        #Return to receive a value
+                        return(pl_surplus_all)    
                 else:
-                   num_1+=1
-                   num_2+=1 
-        # else function
-        else:
-            return(None)           
-
-# print variable conversion1            
-print(conversion1())
+                    # add a value of 1 to num_2 and num_1 variable
+                    num_1+=1
+                    num_2+=1         
+#Activate the Function           
+print(Profit_loss_function())
